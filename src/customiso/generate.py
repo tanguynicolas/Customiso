a3b08b19eeb1bb8c28374fe27e3a3d2c7ffe6b1e
Tanguy <tanguy-nicolas@outlook.fr>
2023-03-20 14:07:52 +0100

    feat improve cli

--- src/customiso/generate.py
+++ src/customiso/generate.py
@@ -7,6 +7,8 @@ import sys
 import os
 import tempfile
 from pathlib import Path
+from getpass import getpass
+
 import yaml
 
 from constants import *
@@ -15,6 +17,8 @@ class Generator:
     def __init__(self, quiet, output):
         self.quiet = quiet
         self.output = output
+        self.yaml_file = ""
+        self.data = {}
 
     def printer(self, normal_str, verbose_str):
         print(normal_str)
@@ -36,37 +40,133 @@ class Generator:
             sys.exit(1)
 
     def init_config(self):
-        global f, content
-
         self.printer("\nQuel sera le nom de votre fichier de configuration ?", 
                     "Vous pouvez spécifier un chemin complet, ou relatif.")
-        f = Path(input(": "))
+        self.yaml_file = Path(input(": ") or "/tmp/customiso.yaml")
+
+        self.yaml_file = self.yaml_file.resolve()
 
         # On ajoute un suffixe .yaml s'il n'y est pas
-        if f.suffix != "yaml" or f.suffix != "yml":
-            f = f.with_suffix(".yaml")
+        if self.yaml_file.suffix != "yaml" or self.yaml_file.suffix != "yml":
+            self.yaml_file = self.yaml_file.with_suffix(".yaml")
 
-        while f.exists():
-            f = Path(input(f"{fCwarning}Le fichier spécifié existe déjà{rC}, veuillez faire un choix alternatif : "))
+        while self.yaml_file.exists():
+            self.yaml_file = Path(input(f"{fCwarning}Le fichier spécifié existe déjà{rC}, veuillez faire un choix alternatif : "))
 
-        f.touch()
-        with open(f, "r") as customiso_yaml:
-            content = yaml.safe_load(customiso_yaml)
+        self.yaml_file.touch()
+
 
     def config_preseed(self):
         self.printer("\n🔹 CONFIGURATION DE L'INSTALLATION SILENCIEUSE", 
                     "Pour toutes les questions, si vous laissez vide, vous serez interrogé par l'installateur au moment de l'installation.")
 
-        self.printer("\nQuelle sera la langue sur le système (format ISO 639-1) ?", 
+        # Localization
+        self.printer("\nQuelle sera la langue pour l'installation (format ISO 639-1) ?", 
                     "Par exemple : en, fr, es, ...")
-        language = input(": ")
-        content["preseed"]["localization"] = "OOOOOOO"
+        language = input(": ") or None
+        
+        self.printer("\nQuelle sera le pays (format ISO 3166-1 alpha-2) ?", 
+                    "Par exemple : UK, FR, ES, ...")
+        county = input(": ") or None
 
-        print(f)
-        #with open(f, "w") as f:
-        #    yaml.safe_dump(content, f, sort_keys=False)
+        self.printer("\nQuelle sera la langue (format <ISO 639-1>_<ISO 3166-1 alpha-2>.<encodage>) ?", 
+                    "Par exemple : en_US.UTF-8, fr_FR.UTF-8, ...")
+        locale = input(": ") or None
 
+        self.printer("\nQuelle sera la disposition du clavier ?", 
+                    "Par exemple : fr(latin9), ...")
+        keymap = input(": ") or None
 
+        # Clock & Time zone
+        self.printer("\nQuelle sera la zone de temps ?", 
+                    "Par exemple : Europe/Paris, ...")
+        timezone = input(": ") or None
+
+        # Network
+        self.printer("\nQuel sera le nom d'hôte ?", 
+                    "Par exemple : customiso-computer.")
+        hostname = input(": ") or None
+
+        self.printer("\nQuel sera le nom de domaine ?", 
+                    "Par exemple : local, u-picardie.fr, ...")
+        domain = input(": ") or None
+
+        self.printer("\nSouhaitez-vous effectuer la configuration IP ?", 
+                    "Si vous ne disposerez pas d'une connexion réseau, choisissez non.")
+        ip_config = input("[o/n] : ")
+        
+        if (ip_config.lower() in ("y", "o")):
+            ip_config = True
+        elif (ip_config.lower() in ("n")):
+            ip_config = False
+        else:
+            ip_config = None
+
+        # Mirror
+        self.printer("\nQuel sera le miroir pour l'installation des paquets ?", 
+                    "Par exemple : http.fr.debian.org, http.us.debian.org, ...")
+        http_hostname = input(": ") or None
+
+        self.printer("\nDans quel répertoire ?????", 
+                    "Recommandé : /debian")
+        http_directory = input(": ") or None
+
+        self.printer("\nQuel est l'adresse du proxy HTTP pour l'installation des paquets ?", 
+                    "Laissez vide si aucun.")
+        http_proxy = input(": ") or None
+
+        # Accounting
+        self.printer("\nSouhaitez-vous activer le compte root ?", 
+                    "Il est recommandé de laisser vide. Dans ce cas, sudo sera utilisé pour l'élévation de privilèges.")
+        root_enable = input("[o/n] : ")
+        
+        if (root_enable.lower() in ("y", "o")):
+            root_enable = True
+            self.printer("\nQuel sera le mot de passe de root ?", 
+                        "Choisissez un mot de passe sécurisé ! Laissez vide si vous souhaitez le demander au moment de l'installation.")
+            root_password = getpass(": ") or None
+        elif (root_enable.lower() in ("y", "o")):
+            root_enable = False
+        else:
+            root_enable = None
+
+        self.printer("\nQuel sera le nom complet de l'utilisateur principal ?", 
+                    "Par exemple : Utilisateur Customiso")
+        user_fullname = input(": ") or None
+
+        self.printer("\nQuel sera le nom d'utilisateur de l'utilisateur principal ?", 
+                    "Par exemple : user-customiso")
+        user_username = input(": ") or None
+
+        self.printer("\nQuel sera le mot de passe de l'utilisateur principal ?", 
+                    "Choisissez un mot de passe sécurisé ! Laissez vide si vous souhaitez le demander au moment de l'installation.")
+        user_password = getpass(": ") or None
+
+        # Partitionning
+        self.printer("\nQuel mode de partitionnement souhaitez-vous utiliser ?", 
+                    """Par exemple 1, 2 ou 3.
+        1 = Tout dans une même partition
+        2 = Partition /homme séparée
+        3 = Partition /home, /var et /tmp séparées.""")
+        partitionning_mode = input(": ") or None
+
+        # Packages
+        self.printer("\nQuels paquets souhaitez-vous installer sur votre système ?", 
+                    "Par exemple : standard, ssh-server, ...")
+        tasksel = input(": ") or None
+
+        self.printer("\nSouaitez-vous participer à l'étude sur les paquets ?", 
+                    "")
+        popularity_contest = input(": ") or None
+
+        # Finishing
+        self.printer("\nSouhaitez-vous faire apparaître le message de fin d'installation ?", 
+                    "Si non, le système redémarrera sans afficher de message.")
+        reboot_message = input(": ") or None
+
+        self.printer("\nSouhaitez-vous que le support d'installation soit éjecté à la fin de l'installation ?", 
+                    "Généralement c'est ce que vous souhaitez pour éviter de démarrer une nouvelle fois sur le support d'installation.")
+        device_eject = input(": ") or None
 
 
     def config_additionnal_packages(self):
---
+++ src/customiso/test.py
@@ -0,0 +1,52 @@
+#import yaml
+#
+#### ÉTAPE 01
+#
+#language = "fr"
+#country = "en"
+#locale = "en_US.UTF-8"
+#keymap = 12
+#
+#content = {
+#    "preseed": {
+#        "localization": {
+#            "language": language,
+#            "country": country,
+#            "locale": locale,
+#            "keymap": keymap
+#        }
+#    }
+#}
+#
+## Écrire le fichier YAML
+#with open("yaml_file.yaml", "w") as f:
+#    yaml.safe_dump(content, f, sort_keys=False)
+#
+#
+#### ÉTAPE 02
+#
+#timezone = "Europe/Paris"
+#
+## Lire le fichier YAML
+#with open("yaml_file.yaml", "r") as f:
+#    content = yaml.safe_load(f)
+#
+## Ajouter les deux dernières lignes
+#content_add = { 
+#    "clock_time_zone": {
+#        "timezone": timezone
+#    }
+#}
+#content["preseed"].update(content_add)
+#
+## Écrire le fichier YAML
+#with open("yaml_file.yaml", "w") as f:
+#    yaml.safe_dump(content, f, sort_keys=False)
+
+from pick import pick
+
+title = 'Please choose your favorite programming language: '
+options = ['Java', 'JavaScript', 'Python', 'PHP', 'C++', 'Erlang', 'Haskell']
+option, index = pick(options, title)
+print(option)
+print(index)
